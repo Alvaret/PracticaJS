beb47e3d2eaa0d7696b52a2521cbd307cdde3c20
Alvaro Ruiz <alvaroruiz.alu@iespacomolla.es>
2024-02-27 14:48:37 +0100

    Añadido evento eliminar

--- js/main.js
+++ js/main.js
@@ -5,5 +5,21 @@ import { discentes , nuevoDiscente , borrarDiscenteId }  from './bibliotecas/obj
 import { cargarDiscentes }  from './bibliotecas/manejoDiscentes.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    var doc = window.document;
+
     cargarDiscentes(discentes);
+
+    //Evento de borrar discente
+    let listado = doc.getElementById("listado");
+    listado.addEventListener('click' , event => {
+        let botonEliminar = event.target.closest('.eliminar');
+        if ( botonEliminar){
+            let idDiscente = botonEliminar.getAttribute('id');
+            borrarDiscenteId(idDiscente);
+            cargarDiscentes(discentes);
+        }
+    })
+
+
+
   });
